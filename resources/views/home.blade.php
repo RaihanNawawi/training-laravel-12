36d4df7d588a980893bca582c8982692ea08e250
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-04-07 13:35:08 +0700

    Fix: Memperbaiki navigasi antar halaman & Membuat halaman blade dan routing nya

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,13 +39,13 @@
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
                             <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                            <a href="#" aria-current="page"
+                            <a href="/" aria-current="page"
                                 class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a>
-                            <a href="#"
+                            <a href="/blog"
                                 class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-                            <a href="#"
+                            <a href="/about"
                                 class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                            <a href="#"
+                            <a href="/contact"
                                 class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
                         </div>
                     </div>
@@ -93,13 +93,13 @@
         <el-disclosure id="mobile-menu" hidden class="block sm:hidden">
             <div class="space-y-1 px-2 pt-2 pb-3">
                 <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                <a href="#" aria-current="page"
+                <a href="/" aria-current="page"
                     class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">Home</a>
-                <a href="#"
+                <a href="/blog"
                     class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-                <a href="#"
+                <a href="/about"
                     class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                <a href="#"
+                <a href="/contact"
                     class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
             </div>
         </el-disclosure>
