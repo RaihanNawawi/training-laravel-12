d9e4dda46655e043b11ea8662e5baee40f13dccb
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-04-07 13:53:28 +0700

    Feat: Membuat blade templating yang bisa menampilkan data yang akan digunakan diberbagai halaman (data statis)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Home - My Laravel Training</title>
+    <title>{{ $title ?? 'My Laravel Training' }}</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
