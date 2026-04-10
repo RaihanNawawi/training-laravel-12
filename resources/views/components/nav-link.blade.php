66208bb7360bc81794137b0495d782506268ba3a
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-04-10 09:24:49 +0700

    Feat: Membuat blade component nav-link & Mengisi navlink dengan class default dan class untuk kondisi active nya

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,7 @@
+<a href="{{ $href }}" class="block rounded-md px-3 py-2 font-medium text-base text-gray-300 hover:bg-white/5 hover:text-white">
+    {{ $slot }}
+</a>
+{{--
+Both: block rounded-md px-3 py-2 font-medium text-base
+Default: text-gray-300 hover:bg-white/5 hover:text-white
+Active: bg-gray-950/50 text-white --}}
