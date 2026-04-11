48810a2ed2235d4cef101c4808f5e38eb5c501b5
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-04-11 09:41:16 +0700

    Refactor & Feat: Membuat ulang component nav-link tanpa class (Anonymous) & Membuat fungsionalitas component nav-link pada tampilan desktop

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,9 @@
-<a href="{{ $href }}" class="block rounded-md px-3 py-2 font-medium text-base text-gray-300 hover:bg-white/5 hover:text-white">
+@props(['href', 'current' => false ])
+
+@php
+    $classes = $current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white'
+@endphp
+
+<a href="{{ $href }}" {{ $attributes->merge(['class' => 'rounded-md px-3 py-2 font-medium text-base ' .$classes]) }}>
     {{ $slot }}
 </a>
-{{--
-Both: block rounded-md px-3 py-2 font-medium text-base
-Default: text-gray-300 hover:bg-white/5 hover:text-white
-Active: bg-gray-950/50 text-white --}}
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -27,14 +27,10 @@
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
                             <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                            <a href="/" aria-current="page"
-                                class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a>
-                            <a href="/blog"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-                            <a href="/about"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                            <a href="/contact"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                            <x-nav-link href="/" :current="request()->is('/')">Home</x-nav-link>
+                            <x-nav-link href="/blog" :current="request()->is('blog')">Blog</x-nav-link>
+                            <x-nav-link href="/about" :current="request()->is('about')">About</x-nav-link>
+                            <x-nav-link href="/contact" :current="request()->is('contact')">Contact</x-nav-link>
                         </div>
                     </div>
                 </div>
@@ -80,7 +76,9 @@
 
         <el-disclosure id="mobile-menu" hidden class="block sm:hidden">
             <div class="space-y-1 px-2 pt-2 pb-3">
-                <x-nav-link href="/">Home</x-nav-link>
+                <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
+                <a href="/" aria-current="page"
+                    class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">Home</a>
                 <a href="/blog"
                     class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
                 <a href="/about"
