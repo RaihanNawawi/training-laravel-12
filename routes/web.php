671cce16b32369693e9ff65bbe066e064f16a9e0
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-04-12 10:20:00 +0700

    chore: Mengganti nama halaman blade blog -> posts

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
 
-Route::get('/blog', function () {
+Route::get('/posts', function () {
     $posts = [
         ['title' => 'How to Build a Laravel Application', 'author' => 'John Doe', 'content' => 'A step-by-step guide to building a Laravel application from scratch, covering everything from setup to deployment. Learn how to create a blog, manage users, and implement authentication using Laravel\'s powerful features.'],
         ['title' => 'Laravel 12 Released: What\'s New?', 'author' => 'Jane Smith', 'content' => 'Laravel 12 has been released with exciting new features and improvements. In this article, we will explore the highlights of Laravel 12, including enhanced performance, new syntax, and improved developer experience']
     ];
 
-    return view('blog', ['title' => 'Blog', 'posts' => $posts]);
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
