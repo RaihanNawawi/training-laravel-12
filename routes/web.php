0534a9f69fe3619dee4c8ac3798a7d42ccb1cf52
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-04-12 10:14:26 +0700

    Feat: Membuat array untuk mengirimkan data ke halaman blog

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
+
 Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+    $posts = [
+        ['title' => 'How to Build a Laravel Application', 'author' => 'John Doe', 'content' => 'A step-by-step guide to building a Laravel application from scratch, covering everything from setup to deployment. Learn how to create a blog, manage users, and implement authentication using Laravel\'s powerful features.'],
+        ['title' => 'Laravel 12 Released: What\'s New?', 'author' => 'Jane Smith', 'content' => 'Laravel 12 has been released with exciting new features and improvements. In this article, we will explore the highlights of Laravel 12, including enhanced performance, new syntax, and improved developer experience']
+    ];
+
+    return view('blog', ['title' => 'Blog', 'posts' => $posts]);
 });
